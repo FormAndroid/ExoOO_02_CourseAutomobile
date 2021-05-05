0f6ada1632a39bdeb2f065a541820cce430b5321
Aurelien <aurelien.strimelle@bstorm.be>
2021-05-05 03:49:09 -0700

    Implemente class Course

--- src/be/bxl/formation/Main.java
+++ src/be/bxl/formation/Main.java
@@ -1,8 +1,37 @@
 package be.bxl.formation;
 
+import be.bxl.formation.models.*;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        Circuit spa = new Circuit("Spa", 7.04);
+
+        // Participant -> Zaza
+        Pilote pilote1 = new Pilote("Zaza", "Vanderquack");
+        Voiture voiture1 = new Voiture("Bugatti", "Veyron", 120, 200);
+        Participant p1 = new Participant(pilote1, voiture1, 42);
+
+        // Participant -> Fifi
+        Pilote pilote2 = new Pilote("Fifi", "Duck");
+        Voiture voiture2 = new Voiture("VW", "Polo", 90, 140);
+        Participant p2 = new Participant(pilote2, voiture2, 2);
+
+        // Course
+        Course c1 = new Course("Demo !", spa, 5);
+        c1.addParticipant(p1);
+        c1.addParticipant(p2);
+
+        // Participant -> Balthazar
+        Pilote pilote3 = new Pilote("Balthazar", "Picsou");
+        Voiture voiture3 = new Voiture("VW", "Coccinelle", 75, 130);
+        c1.addParticipant(pilote3, voiture3, 1337);
+
+        // Start de la course
+        c1.simulation();
+
+        // Affichage du resultat
+        // c1.obtenirVainqueur
     }
 }
--- src/be/bxl/formation/models/Course.java
+++ src/be/bxl/formation/models/Course.java
@@ -1,4 +1,93 @@
 package be.bxl.formation.models;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Course {
+
+    public final int NOMBRE_MAX_PARTICIPANT = 10;
+
+    //region Champs
+    private String nom;
+    private Circuit circuit;
+    private int nombreTour;
+    private ArrayList<Participant> participants;
+    //endregion
+
+
+    //region Encapsulation
+    public String getNom() {
+        return nom;
+    }
+
+    public Circuit getCircuit() {
+        return circuit;
+    }
+
+    public int getNombreTour() {
+        return nombreTour;
+    }
+
+    public List<Participant> getParticipants() {
+        return Collections.unmodifiableList(participants);
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setCircuit(Circuit circuit) {
+        this.circuit = circuit;
+    }
+
+    public void setNombreTour(int nombreTour) {
+        this.nombreTour = nombreTour;
+    }
+    //endregion
+
+
+    //region Constructure
+    public Course(String nom, Circuit circuit, int nombreTour) {
+        setNom(nom);
+        setCircuit(circuit);
+        setNombreTour(nombreTour);
+
+        this.participants = new ArrayList<>();
+    }
+    //endregion
+
+
+    //region Méthode
+    public void addParticipant(Participant participant) {
+        // TODO Verifier si les numeros des participants sont unique
+        //      Alternative, la course genere le numero
+
+        this.participants.add(participant);
+    }
+
+    public void addParticipant(Pilote pilote, Voiture voiture, int numero){
+        Participant p = new Participant(pilote, voiture, numero);
+        this.addParticipant(p);
+    }
+
+    public void simulation() {
+        int tourActuel = 0;
+
+//        System.out.println("Simulation de la cours");
+//        System.out.println();
+        while(tourActuel < this.nombreTour) {
+            tourActuel++;
+
+//            System.out.println();
+//            System.out.println(" - Tour " + tourActuel);
+            for(Participant p : this.participants) {
+                p.faireUnTour(this.circuit);
+
+//                double temps = p.getTempsTour().get(p.getTempsTour().size() - 1);
+//                System.out.println(p.getNumero() + " -> " + temps);
+            }
+        }
+    }
+    //endregion
 }
